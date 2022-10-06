de47409e5bf4e8a80de77fe7a48cb13ceb17740c
Brcok7890 <nm1107@student.aru.ac.uk>
2022-10-06 13:41:43 +0100

    v1.1 ifEx2 - added loop

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-﻿int x,y,z,pn=0;
+﻿char choice;
+do{
+int x, y, z, pn = 0;
 Console.WriteLine("Enter 3 real numbers pls: ");
 x = Convert.ToInt32(Console.ReadLine());
-y= Convert.ToInt32(Console.ReadLine());
+y = Convert.ToInt32(Console.ReadLine());
 z = Convert.ToInt32(Console.ReadLine());
-if (x<0){pn = pn+1;}
-if (y<0){pn = pn+1;}
-if (z<0){pn = pn+1;}
+if (x < 0) { pn = pn + 1; }
+if (y < 0) { pn = pn + 1; }
+if (z < 0) { pn = pn + 1; }
 Console.WriteLine(pn);
-if (pn == 0 || pn == 2){Console.WriteLine("Your number is postitive");}
-else if (pn == 1 || pn == 3){Console.WriteLine("Your number is negative");}
-Console.ReadKey();
+if (pn == 0 || pn == 2) { Console.WriteLine("Your number is postitive"); }
+else if (pn == 1 || pn == 3) { Console.WriteLine("Your number is negative"); }
+Console.WriteLine("would you like to repeat the program? (press n to end): ");
+choice = Convert.ToChar(Console.ReadLine());
+}
+while(choice != 'n');
